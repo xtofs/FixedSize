c185c470a46159f15aeed3e9bc83cce5c7d2bf9b
Christof <christof_sprenger@hotmail.com>
2024-01-17 11:44:13 -0800

    explain concat limitation

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Size1 = ConstSize.S<ConstSize.Z>;
 using Size2 = ConstSize.S<ConstSize.S<ConstSize.Z>>;
 using Size3 = ConstSize.S<ConstSize.S<ConstSize.S<ConstSize.Z>>>;
 using Size4 = ConstSize.S<ConstSize.S<ConstSize.S<ConstSize.S<ConstSize.Z>>>>;
+using Size5 = ConstSize.S<ConstSize.S<ConstSize.S<ConstSize.S<ConstSize.S<ConstSize.Z>>>>>;
 
 internal class Program
 {
@@ -23,7 +24,13 @@ internal class Program
         // Console.WriteLine(c);
 
 
+        // FixedSizeVector<int, Sum<Size2, Size3>
         var y = b.Concat(c);
         Console.WriteLine(y);
+
+        var d = new FixedSizeVector<int, Size5>(1, 2, 3, 4, 5);
+        // ERROR: var x = y.Zip(d);
+        // even though y's type FixedSizeVector<int, Sum<Size2, Size3>
+        // and d's type Size5 both represent size 5, C# doesn't know they are identical.
     }
 }
