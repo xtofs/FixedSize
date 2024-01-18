982f241fc52992c237c651deb7bc66fb8ea3fd1d
Christof <christof_sprenger@hotmail.com>
2024-01-17 18:08:01 -0800

    removed printing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using ConstSize;
 
@@ -13,15 +14,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var a = WriteExpression("a", new FixedSizeVector<string, Size2>("a", "b"));
-        var b = WriteExpression("b", new FixedSizeVector<int, Size2>(1, 2));
-        var c = WriteExpression("c", new FixedSizeVector<int, Size3>(3, 4, 5));
+        var a = new FixedSizeVector<string, Size2>("a", "b");
+        var b = new FixedSizeVector<int, Size2>(1, 2);
+        var c = new FixedSizeVector<int, Size3>(3, 4, 5);
 
-        var z = WriteExpression("z", a.Zip(b));
+        var x = a.Zip(b);
 
-        // ERROR:  var x = a.Zip(c);
+        // var y = a.Zip(c);
+        // ERROR: wrong size
+        // The type arguments for method 'Extensions.Zip<A, B, TSize>(FixedSizeVector<A, TSize>, FixedSizeVector<B, TSize>)' cannot be inferred from the usage. Try specifying the type arguments explicitly.CS0411
 
-        var y = WriteExpression("y", b.Concat(c));
+        var y = b.Concat(c);
 
         var d = new FixedSizeVector<int, Size5>(1, 2, 3, 4, 5);
         // ERROR: var x = y.Zip(d);
@@ -30,20 +33,7 @@ internal class Program
         // C# doesn't know the sizes are the same because Sum<Size2,Size3> != Size5 == S<S<S<S<S<Z>>>>>
         // even though both types static .Value propert returns 5.
 
-        WriteExpression(Sum<Size2, Size3>.Value);
-        WriteExpression(Size5.Value);
-
-    }
-
-    private static T WriteExpression<T>(T value, [CallerArgumentExpression("value")] string? arg = null)
-    {
-        Console.WriteLine("{0} = {1}", arg, value);
-        return value;
-    }
-
-    private static T WriteExpression<T>(string variable, T value, [CallerArgumentExpression(nameof(value))] string? arg = null)
-    {
-        Console.WriteLine("{0} = {1} = {2}", variable, arg, value);
-        return value;
+        Debug.Assert(5 == Sum<Size2, Size3>.Value);
+        Debug.Assert(5 == Size5.Value);
     }
 }
